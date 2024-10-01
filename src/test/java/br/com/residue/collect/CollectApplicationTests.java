fc7e2c12c1b2f501fc175bd78fdbf6a67676cd7b
Antonio <pisouza011@gmail.com>
2024-09-30 21:20:49 -0300

    Continuous Integration

--- src/test/java/br/com/residue/collect/CollectApplicationTests.java
+++ src/test/java/br/com/residue/collect/CollectApplicationTests.java
@@ -1,10 +1,21 @@
 package br.com.residue.collect;
 
+import br.com.residue.collect.infra.security.TokenService;
+import br.com.residue.collect.infra.security.VerifyToken;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CollectApplicationTests {
+
+	@MockBean
+	private TokenService tokenService;
+
+	@MockBean
+	private VerifyToken verifyToken;
 
 	@Test
 	void contextLoads() {
