c43e7d87e6d5db1e1468b2eb418225d12b071800
Antonio <pisouza011@gmail.com>
2024-10-05 18:41:59 -0300

    Fix: improvements on ci-cd prod and dev enviroments

--- src/main/resources/db/migrations/prod/V5__create_table_users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tbl_users (
-                           user_id UUID PRIMARY KEY,
-                           nome VARCHAR(100) NOT NULL,
-                           email VARCHAR(100) UNIQUE NOT NULL,
-                           senha VARCHAR(100) NOT NULL,
-                           role VARCHAR(50) NOT NULL DEFAULT 'USER'
-);
